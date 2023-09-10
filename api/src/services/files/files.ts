acea82e3a92f4ee786fbfcf84ab39ef7257f5f1b
Rafael de Souza <rafaeldesouzav@gmail.com>
2023-09-10 14:43:30 -0300

    feat: Prevent duplicate files

--- api/src/services/files/files.ts
+++ api/src/services/files/files.ts
@@ -1,6 +1,9 @@
 import type { QueryResolvers, MutationResolvers } from 'types/graphql'
+import { Prisma } from '@prisma/client'
+import { RedwoodError } from '@redwoodjs/api'
 
 import { db } from 'src/lib/db'
+import { logger } from 'src/lib/logger'
 
 export const files: QueryResolvers['files'] = () => {
   return db.file.findMany()
@@ -12,17 +15,47 @@ export const file: QueryResolvers['file'] = ({ id }) => {
   })
 }
 
-export const createFile: MutationResolvers['createFile'] = ({ input }) => {
-  return db.file.create({
-    data: input,
-  })
+export const createFile: MutationResolvers['createFile'] = async ({ input }) => {
+  try {
+    return await db.file.create({
+      data: input,
+    })
+  } catch (error) {
+    logger.error(error, 'Error creating file')
+
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      // P2022: Unique constraint failed
+      // Prisma error codes: https://www.prisma.io/docs/reference/api-reference/error-reference#error-codes
+      if (error.code === 'P2002') {
+        logger.error('The file already exists', error)
+        throw new RedwoodError('The file already exists')
+      }
+    }
+
+    throw error
+  }
 }
 
-export const updateFile: MutationResolvers['updateFile'] = ({ id, input }) => {
-  return db.file.update({
-    data: input,
-    where: { id },
-  })
+export const updateFile: MutationResolvers['updateFile'] = async ({ id, input }) => {
+  try {
+    return await db.file.update({
+      data: input,
+      where: { id },
+    })
+  } catch (error) {
+    logger.error(error, 'Error updating file')
+
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      // P2022: Unique constraint failed
+      // Prisma error codes: https://www.prisma.io/docs/reference/api-reference/error-reference#error-codes
+      if (error.code === 'P2002') {
+        logger.error('The file already exists', error)
+        throw new RedwoodError('The file already exists')
+      }
+    }
+
+    throw error
+  }
 }
 
 export const deleteFile: MutationResolvers['deleteFile'] = ({ id }) => {
